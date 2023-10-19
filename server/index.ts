5b56139dff770b53c2f8ccd33387afa214cfe541
Doubleneck <antti.vuorenmaa@kolumbus.fi>
2023-10-19 19:30:44 +0300

    exercise 9.6

--- server/index.ts
+++ server/index.ts
@@ -1,7 +1,8 @@
 import express from 'express';
 import calculateBmi from './bmiCalculator';
+import { calculator } from './calculator';
 const app = express();
-
+app.use(express.json());
 
 app.get('/ping', (_req, res) => {
     res.send('pong');
@@ -14,7 +15,7 @@ app.get('/hello', (_req, res) => {
 app.get('/bmi', (_req, res) => {
     const height = Number(_req.query.height);
     const weight = Number(_req.query.weight);
-  
+
     if (isNaN(height) || isNaN(weight)) {
         res.status(400).json({ error: 'malformatted parameters' });
         return;
@@ -25,6 +26,17 @@ app.get('/bmi', (_req, res) => {
         bmi: calculateBmi(height, weight)
     });
   });
+
+app.post('/calculate', (_req, res) => {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment 
+    const { value1, value2, op } = _req.body;
+    if ( !value1 || !value2 ||isNaN(Number(value1))||isNaN(Number(value2) ) ) {
+        return res.status(400).send({ error: '...'});
+    }
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+    const result = calculator(Number(value1), Number(value2), op);
+    return res.send({ result });
+  });  
 
 const PORT = 3003;
 
