536efea74ab5cf791751684572cf39fa6a7852f0
Doubleneck <antti.vuorenmaa@kolumbus.fi>
2023-10-24 14:01:43 +0300

    fix structure in Patient/server: put index.ts inside src

--- Patientor/server/src/index.ts
+++ Patientor/server/src/index.ts
@@ -2,8 +2,8 @@
 import express from 'express';
 import cors from 'cors';
 const app = express();
-import diagnoseRouter from './src/routes/diagnoses_route';
-import patientRouter from './src/routes/patients_route';
+import diagnoseRouter from './routes/diagnoses_route';
+import patientRouter from './routes/patients_route';
 app.use(express.json());
 // eslint-disable-next-line @typescript-eslint/no-unsafe-call
 app.use(cors());
